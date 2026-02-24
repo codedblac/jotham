1be43a6d6b6b37c41d75738b9632a7fa5691aded
CODED BLACK <codedb693@gmail.com>
2026-02-24 18:41:51 +0300

    Update setup-backend.sh

--- setup-backend.sh
+++ setup-backend.sh
@@ -24,6 +24,7 @@ mkdir -p src/routes src/controllers src/config src/middleware
 # src/index.js
 ########################################
 
+
 cat <<'EOL' > src/index.js
 import express from 'express';
 import cors from 'cors';
